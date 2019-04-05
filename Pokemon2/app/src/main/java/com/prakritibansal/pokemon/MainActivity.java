00d94a058d14188db7a63791e3a0ac780f1d7880
Prakriti Bansal <prakriti_bansal@yahoo.com>
2019-04-06 03:37:59 +0530

    add trained model

--- Pokemon2/app/src/main/java/com/prakritibansal/pokemon/MainActivity.java
+++ Pokemon2/app/src/main/java/com/prakritibansal/pokemon/MainActivity.java
@@ -1,13 +1,37 @@
 package com.prakritibansal.pokemon;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.go);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Fragment fragment = new Camera2BasicFragment();
+//                Bundle args = new Bundle();
+//                fragment.setArguments(args);
+//
+//                FragmentManager fragmentManager = getFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                fragmentTransaction.replace(R.id.frameLayout, fragment);
+//                fragmentTransaction.addToBackStack(null);
+//                fragmentTransaction.commit();
+            }
+        });
     }
 }
